764522320c425360f4c3913613443719c27b91e3
Omar <omarudiazz@outlook.com>
2026-04-27 20:22:52 -0600

    Añadimos logica matematica y conectamos los controles con las Matematicas

--- funciones.js
+++ funciones.js
@@ -22,7 +22,6 @@ function validarYActualizar(idArticulacion, valor, origen) {
 
     let numVal = parseFloat(valor);
 
-    // Validación de la ventana emergente (Cumpliendo el inciso e)
     if (numVal < min || numVal > max) {
         alert(`¡Advertencia! El valor ingresado para ${idArticulacion} está fuera de los rangos permitidos (${min} a ${max}).`);
         
@@ -30,18 +29,16 @@ function validarYActualizar(idArticulacion, valor, origen) {
         if (origen === 'input') {
             input.value = slider.value;
         }
-        return; // Detenemos la ejecución
+        return; 
     }
 
-    // Si todo está correcto, sincronizamos slider y input
     if (origen === 'slider') {
         input.value = numVal;
     } else if (origen === 'input') {
         slider.value = numVal;
     }
 
-    // TODO: Aquí llamaremos más adelante a la función de la matriz cinemática
-    // calcularCinematica();
+    calcularCinematica();
 }
 
 
@@ -54,3 +51,43 @@ q2_input.addEventListener('change', (e) => validarYActualizar('q2', e.target.val
 
 q3_slider.addEventListener('input', (e) => validarYActualizar('q3', e.target.value, 'slider'));
 q3_input.addEventListener('change', (e) => validarYActualizar('q3', e.target.value, 'input'));
+
+
+//Motor cinematico
+
+
+// altura inicial y valor de la pinza en cm
+const L1 = 50; 
+const LONGITUD_PINZA = 8; 
+
+function gradosARadianes(grados) {
+    return grados * (Math.PI / 180);
+}
+
+// Función que calcula la posición X, Y, Z
+function calcularCinematica() {
+    // 1. Leer los valores actuales
+    let q1_grados = parseFloat(q1_input.value);
+    let q2_grados = parseFloat(q2_input.value);
+    let q3_cm = parseFloat(q3_input.value);
+
+    // 2. Convertir ángulos a radianes
+    let q1 = gradosARadianes(q1_grados);
+    let q2 = gradosARadianes(q2_grados);
+
+    // 3. Calcular la extensión total (q3 + valor pinza)
+    let q3_total = q3_cm + LONGITUD_PINZA;
+
+    // 4. Aplicar las ecuaciones de la matriz T3 (Última columna)
+    let x = q3_total * Math.cos(q1) * Math.sin(q2);
+    let y = q3_total * Math.sin(q1) * Math.sin(q2);
+    let z = (q3_total * Math.cos(q2)) + L1;
+
+    // 5. Mostrar los resultados en el HTML (redondeando a 2 decimales para que se vea limpio)
+    document.getElementById('posX').innerText = x.toFixed(2);
+    document.getElementById('posY').innerText = y.toFixed(2);
+    document.getElementById('posZ').innerText = z.toFixed(2);
+}
+
+// Calcular la posición inicial al cargar la página (Inciso d)
+calcularCinematica();
